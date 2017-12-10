a10c14843abbbdcf1cd4d0cfd06e7bfece916345
Parithat <starsis25391@hotmail.com>
2017-12-10 17:21:50 +0700

    update part9

---
+++ BLOGSApp/app/src/main/java/com/example/win81/blogsapp/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.example.win81.blogsapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mLoginEmailField;
+    private EditText mLoginPasswordField;
+    private Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgress;
+    private DatabaseReference mDatabaseUsers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabaseUsers= FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseUsers.keepSynced(true);
+
+        mProgress = new ProgressDialog(this);
+
+        mLoginPasswordField = (EditText) findViewById(R.id.loginPasswordField);
+        mLoginEmailField  = (EditText) findViewById(R.id.loginEmailField);
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+        
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkLogin();
+            }
+        });
+        
+    }
+
+    private void checkLogin() {
+
+        String email = mLoginEmailField.getText().toString().trim();
+        String password = mLoginPasswordField.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+
+            mProgress.setMessage("Checking Login ...");
+            mProgress.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+
+                        mProgress.dismiss();
+
+                        checkUserExist();
+
+                    }
+                    else{
+                        mProgress.dismiss();
+                        Toast.makeText(LoginActivity.this,"Error Login ...",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
+
+    }
+
+    private void checkUserExist() {
+
+        final String user_id = mAuth.getCurrentUser().getUid();
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.hasChild(user_id)){
+
+                    Intent mainIntent = new Intent(LoginActivity.this , MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(mainIntent);
+
+                }else {
+                    Intent setupIntent = new Intent(LoginActivity.this , SetupActivity.class);
+                    setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(setupIntent);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
---
+++ BLOGSApp/app/src/main/java/com/example/win81/blogsapp/MainActivity.java
@@ -0,0 +1,177 @@
+package com.example.win81.blogsapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mBlogList;
+    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabaseUsers;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() == null){
+
+                    Intent loginIntent = new Intent(MainActivity.this , LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+
+                }
+
+            }
+        };
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseUsers.keepSynced(true);
+
+        mBlogList = (RecyclerView) findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        checkUserExist();
+        mAuth.addAuthStateListener(mAuthListener);
+
+        FirebaseRecyclerAdapter<Blog, BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(
+
+                Blog.class,
+                R.layout.blog_row,
+                BlogViewHolder.class,
+                mDatabase
+
+        ) {
+            @Override
+            protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
+
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDesc());
+                viewHolder.setImage(getApplicationContext() , model.getImage());
+
+
+            }
+        };
+
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    private void checkUserExist() {
+
+        final String user_id = mAuth.getCurrentUser().getUid();
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (!dataSnapshot.hasChild(user_id)){
+
+                    Intent setupIntent = new Intent(MainActivity.this , SetupActivity.class);
+                    setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(setupIntent);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+        public void setTitle(String title){
+
+            TextView post_title = (TextView) mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+
+        }
+        public void setDesc(String desc){
+
+            TextView post_desc = (TextView) mView.findViewById(R.id.post_desc);
+            post_desc.setText(desc);
+
+        }
+        public void setImage(Context ctx ,String image){
+
+            ImageView post_image = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_add){
+
+            startActivity(new Intent(MainActivity.this , PostActivity.class));
+
+        }
+        if (item.getItemId() == R.id.action_logout){
+
+            logout();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mAuth.signOut();
+    }
+}
